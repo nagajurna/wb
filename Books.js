33c73596bc7119e41be9e4d613d93c263c0135c1
Paul <ppruneta@gmail.com>
2017-10-19 18:47:32 +0200

    ie problem

--- Books.js
+++ Books.js
@@ -1,5 +1,6 @@
 const Books = [
 	{ id: 1, title: 'Trois&nbsp;discours&nbsp;sur&nbsp;la&nbsp;condition des&nbsp;grands', author: 'Blaise Pascal', path: '/books/trois_discours_sur_la_condition_des_grands' },
-	{ id: 2, title: 'Fragment d\'un&nbsp;traité du&nbsp;vide', author: 'Blaise Pascal', path: '/books/preface_traite_vide' }
+	{ id: 2, title: 'Fragment d\'un&nbsp;traité du&nbsp;vide', author: 'Blaise Pascal', path: '/books/preface_traite_vide' },
+	{ id: 3, title: 'Vies imaginaires', author: 'Marcel Schwob', path: '/books/vies_imaginaires'}
 ]
 module.exports = Books;
