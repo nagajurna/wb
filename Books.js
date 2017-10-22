f961a68841773e506d36d10511a6ecb80b7c6d8d
Paul <ppruneta@gmail.com>
2017-10-22 10:39:05 +0200

    removed a book

--- Books.js
+++ Books.js
@@ -1,7 +1,7 @@
 const Books = [
 	{ id: 1, title: 'Trois&nbsp;discours&nbsp;sur&nbsp;la&nbsp;condition des&nbsp;grands', author: 'Blaise Pascal', path: '/books/trois_discours_sur_la_condition_des_grands' },
 	{ id: 2, title: 'Vies imaginaires', author: 'Marcel Schwob', path: '/books/vies_imaginaires'},
-	{ id: 3, title: 'La Croisade des enfants', author: 'Marcel Schwob', path: '/books/la_croisade_des_enfants'},
-	{ id: 4, title: 'La Grande beuverie', author: 'René Daumal', path: '/books/la_grande_beuverie'}
+	{ id: 3, title: 'La Croisade des enfants', author: 'Marcel Schwob', path: '/books/la_croisade_des_enfants'}
+	//{ id: 4, title: 'La Grande beuverie', author: 'René Daumal', path: '/books/la_grande_beuverie'}
 ]
 module.exports = Books;
